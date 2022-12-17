5ef7cdccddf18da5ca9541aa6be7a6afdaac0f2f
ganariya <ganariya2525@gmail.com>
2022-12-17 15:34:59 +0900

    :sparkles: add service

--- pkg/domain/interactor/user_create.go
+++ pkg/domain/interactor/user_create.go
@@ -1,19 +1,26 @@
 package interactor
 
 import (
+	"fmt"
+
 	"github.com/ganyariya/itddd_go/pkg/boundary/input"
 	"github.com/ganyariya/itddd_go/pkg/boundary/irepository"
 	"github.com/ganyariya/itddd_go/pkg/boundary/output"
 	"github.com/ganyariya/itddd_go/pkg/domain/entity"
+	"github.com/ganyariya/itddd_go/pkg/domain/service"
 	"github.com/ganyariya/itddd_go/pkg/domain/value"
 )
 
 type UserCreateInteractor struct {
 	userRepository irepository.IUserRepository
+	userService    *service.UserService
 }
 
-func NewUserCreateInteractor(userRepo irepository.IUserRepository) *UserCreateInteractor {
-	return &UserCreateInteractor{userRepository: userRepo}
+func NewUserCreateInteractor(userRepo irepository.IUserRepository, userServiece *service.UserService) *UserCreateInteractor {
+	return &UserCreateInteractor{
+		userRepository: userRepo,
+		userService:    userServiece,
+	}
 }
 
 func (u *UserCreateInteractor) Handle(inputData *input.UserCreateInputData) (*output.UserCreateOutputData, error) {
@@ -22,6 +29,11 @@ func (u *UserCreateInteractor) Handle(inputData *input.UserCreateInputData) (*ou
 		return nil, err
 	}
 	user := entity.NewUser(userId, inputData.UserName)
+
+	if u.userService.Exists(user) {
+		return nil, fmt.Errorf("already user exists")
+	}
+
 	err = u.userRepository.Save(user)
 	if err != nil {
 		return nil, err
--- pkg/domain/interactor/user_create_test.go
+++ pkg/domain/interactor/user_create_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ganyariya/itddd_go/pkg/boundary/input"
 	"github.com/ganyariya/itddd_go/pkg/domain/entity"
 	"github.com/ganyariya/itddd_go/pkg/domain/interactor"
+	"github.com/ganyariya/itddd_go/pkg/domain/service"
 	"github.com/ganyariya/itddd_go/pkg/domain/value"
 	"github.com/ganyariya/itddd_go/pkg/infrastructure/stub"
 	"github.com/stretchr/testify/assert"
@@ -14,9 +15,8 @@ import (
 func TestUserCreate(t *testing.T) {
 	rawUserId := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 	stubRepo := stub.NewUserStubRepository([]*entity.User{})
-	interactor := interactor.NewUserCreateInteractor(
-		stubRepo,
-	)
+	service := service.NewUserService(stubRepo)
+	interactor := interactor.NewUserCreateInteractor(stubRepo, service)
 	inputData := input.NewUserCreateInputData(rawUserId, "ganyariya")
 
 	outputData, err := interactor.Handle(inputData)
@@ -28,4 +28,18 @@ func TestUserCreate(t *testing.T) {
 	founded, err := stubRepo.Find(userId)
 	assert.NoError(t, err)
 	assert.Equal(t, "ganyariya", founded.Name)
+}
+
+func TestUserCreateDuplicateError(t *testing.T) {
+	rawUserId := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	stubRepo := stub.NewUserStubRepository([]*entity.User{})
+	service := service.NewUserService(stubRepo)
+	interactor := interactor.NewUserCreateInteractor(stubRepo, service)
+	inputData := input.NewUserCreateInputData(rawUserId, "ganyariya")
+
+	_, err := interactor.Handle(inputData)
+	assert.NoError(t, err)
+
+	_, err = interactor.Handle(inputData)
+	assert.Error(t, err)
 }
